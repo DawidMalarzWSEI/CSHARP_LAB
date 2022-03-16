af093e0ba9e9b2164e0991c46513e1e07c518b19
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-03-16 18:20:55 +0100

    Task 3 done. Operator overload

--- Lab1/Fraction.cs
+++ Lab1/Fraction.cs
@@ -20,6 +20,25 @@ namespace Lab1
             gauge = fraction.gauge;
             denominator = fraction.denominator;
         }
+        public static Fraction operator +(Fraction a) => a;
+
+        public static Fraction operator -(Fraction a) => new(-a.gauge, a.denominator);
+
+        public static Fraction operator +(Fraction a, Fraction b) =>
+               new(a.gauge * b.denominator + b.gauge * a.denominator, a.denominator * b.denominator);
+
+        public static Fraction operator *(Fraction a, Fraction b) =>
+                new(a.gauge * b.gauge, a.denominator * b.denominator);
+
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            if (b.gauge == 0)
+            {
+                throw new DivideByZeroException();
+            }
+
+            return new Fraction(a.gauge * b.denominator, a.denominator * b.gauge);
+        }
         public override string ToString() => $"{gauge} / {denominator}";
 
     }
