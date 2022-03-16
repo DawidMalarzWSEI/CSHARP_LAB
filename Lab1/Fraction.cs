adf8caac57942a0e704f4922ffb2090cbf86d0d7
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-03-16 18:13:34 +0100

    Task 1 done. Create basic Fraction class

---
+++ Lab1/Fraction.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1
+{
+    class Fraction
+    {
+        private int gauge = 0;
+        private int denominator = 1;
+
+        public Fraction(int gauge, int denominator)
+        {
+            this.gauge = gauge;
+            this.denominator = denominator;
+        }
+
+        public Fraction(Fraction fraction)
+        {
+            gauge = fraction.gauge;
+            denominator = fraction.denominator;
+        }
+
+    }
+}
