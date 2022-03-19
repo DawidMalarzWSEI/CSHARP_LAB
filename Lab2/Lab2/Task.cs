f6d9c72e5a06852495399480a5398ec70de988ac
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-03-19 15:25:49 +0100

    Implement Task class

---
+++ Lab2/Lab2/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    enum TaskStatus
+    {
+        Waiting,
+        Progress,
+        Done,
+        Rejected
+    }
+
+
+    class Task
+    {
+        private string name;
+        private TaskStatus status;
+        public string Name { get => name; }
+        public TaskStatus Status { get => status; set => status = value; }
+
+        public Task(string name, TaskStatus status)
+        {
+            this.name = name;
+            this.status = status;
+        }
+
+        public override string ToString()
+        {
+            return $"Tasks: {name} [{status}]";
+        }
+    }
+}
