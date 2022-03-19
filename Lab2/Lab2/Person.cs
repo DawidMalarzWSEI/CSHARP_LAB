37f01d326bcf26abc45d1e693fe154385bc3c300
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-03-19 15:03:52 +0100

    Lab2 - Person Class done

---
+++ Lab2/Lab2/Person.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Person : IEquatable<Person>
+    {
+        protected string name;
+        protected int age;
+
+        public string Name { get => name; }
+        public int Age { get => age; }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public bool Equals(Person other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            if (this.name == other.name && this.age == other.age)
+                return true;
+            return false;
+        }
+        public override string ToString()
+        {
+            return $"Name: {this.name} Age: {this.age}";
+        }
+    }
+
+}
