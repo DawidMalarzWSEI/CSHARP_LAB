3a2bda035cfa8a0078cdcf642a22db419587a525
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-04-01 14:21:36 +0200

    Add Fruit and Meat class

---
+++ lab_2_zadanie/Fruit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace lab_2_zadanie
+{
+    class Fruit : Product
+    {
+        private int count;
+
+        public int Count { get => count; }
+
+        public Fruit(string name, int count) : base(name)
+        {
+            this.count = count;
+        }
+        public override void Print(string prefix = "\t")
+        {
+            Console.WriteLine(prefix + this.Name + " (" + this.count + " " + (this.count == 1 ? "fruit" : "fruits") + ")"); 
+        }
+    }
+}
---
+++ lab_2_zadanie/Meat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lab_2_zadanie
+{
+    class Meat : Product
+    {
+        private double weight;
+
+        public double Weight { get => weight; }
+
+        public Meat(string name, double weight) : base(name)
+        {
+            this.weight = weight;
+        }
+
+        public override void Print(string prefix = "\t")
+        {
+            Console.WriteLine(prefix + this.Name + " (" + this.weight + " kg)");
+        }
+    }
+}
