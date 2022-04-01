a856b316e44bb5bdeea190f2a7f196b0fcb9f4e9
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-04-01 14:22:32 +0200

    Implement Buyer and Seller class

---
+++ lab_2_zadanie/Buyer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_zadanie
+{
+    class Buyer : Person
+    {
+        protected new List<Product> tasks = new List<Product>();
+        public Buyer(string name, int age) : base(name, age) {; }
+        public void AddProduct(Product product) {
+            tasks.Add(product);
+        }
+        public void RemoveProduct(int index)
+        {
+            tasks.RemoveAt(index);
+        }
+        public override void Print(string prefix = "\t")
+        {
+            Console.WriteLine(prefix + "Buyer: " + this.Name + " (" + this.Age.ToString() + " y.o.)");
+
+            if (tasks.Count > 0)
+            {
+                Console.WriteLine($"{prefix}{prefix}-- Products: --");
+                foreach (Product product in tasks)
+                {
+                    Console.Write(prefix);
+                    product.Print();
+                }
+            }
+           
+        }
+    }
+}
---
+++ lab_2_zadanie/Seller.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace lab_2_zadanie
+{
+    class Seller : Person
+    {
+        public Seller(string name, int age) : base(name, age) {;}
+        public override void Print(string prefix = "\t")
+        {
+            Console.WriteLine(prefix + "Seller: " + this.Name + " (" + this.Age.ToString() + " y.o.)");
+        }
+    }
+}
