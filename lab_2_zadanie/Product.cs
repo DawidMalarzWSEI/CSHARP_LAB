97ff717301d7ce36038dee9e1642c1437ce112db
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-04-01 14:20:33 +0200

    Add Product class

---
+++ lab_2_zadanie/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace lab_2_zadanie
+{
+    class Product : IThing
+    {
+        private string name;
+        public string Name { get => name; }
+
+        public Product(string name)
+        {
+            this.name = name;
+        }
+
+        public virtual void Print(string prefix = "\t")
+        {
+            Console.Write($"{prefix} {this.name}");
+        }
+    
+        
+    }
+}
