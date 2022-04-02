bfb58907beecc20ecf65e26609827c8ad11ef2ee
DawidMalarzWSEI <dawid.malarz@microsoft.wsei.edu.pl>
2022-04-02 15:57:18 +0200

    Add base clasess

---
+++ lab3/lab3/Logger/WriterLogger.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace ConsoleApp.Logger
+{
+    public abstract class WriterLogger : ILogger
+    {
+        protected TextWriter writer;
+
+        public virtual void Log(params string[] messages)
+        {
+            // Uzupełnić to miejsce o logikę zapisu opartą o TextWriter ...
+        }
+
+        public abstract void Dispose();
+    }
+}
