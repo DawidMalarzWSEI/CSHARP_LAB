c756a544d6718f97a047309d1c8cfca7f2d31923
Kacper Kita <kkita.contact@gmail.com>
2022-04-21 17:20:17 +0200

    ConsoleLogger

--- lab3/lab3/Logger/ConsoleLogger.cs
+++ lab3/lab3/Logger/ConsoleLogger.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ConsoleApp.Logger;
 
 namespace lab3.Logger
 {
-    class ConsoleLogger
+    class ConsoleLogger : WriterLogger
     {
+        public ConsoleLogger()
+        {
+
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
